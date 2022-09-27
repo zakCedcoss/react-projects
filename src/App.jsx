ad46c5dc9c2ee471e16a1f1afdd7e2385c310d8e
Zak <zeeshan@cedcommerce.com>
2022-09-27 18:00:59 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import "./App.css";
-import Cart from "./components/Cart";
 // import ColorChangingText from "./components/ColorChangingText";
-// import ReactContext from "./components/ReactContext";
-// import Sidebar from "./components/Sidebar";
-import { Route, Routes } from "react-router-dom";
-import SingleProduct from "./components/Cart/SingleProduct";
+import ReactContext from "./components/ReactContext";
 
 function App() {
   return (
     <div className="App">
       {/* <ColorChangingText/> */}
       {/* <Sidebar/> */}
-      {/* <ReactContext/> */}
-      <Routes>
-        <Route path="/" element={<Cart />} />
-        <Route path="/:id" element={<SingleProduct />} />
-      </Routes>
+      <ReactContext />
     </div>
   );
 }
